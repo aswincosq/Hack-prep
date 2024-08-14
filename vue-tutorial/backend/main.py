5be621ba1cce8e7fd4ef9c4e5a7788bb775d4f7c
AllenTJ7 <163137620+AllenTJ7@users.noreply.github.com>
2024-08-14 11:30:38 +0530

    basic setup

---
+++ vue-tutorial/backend/main.py
@@ -0,0 +1,15 @@
+from flask_cors import CORS
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+CORS(app,resources={r"/*":{'origins':"*"}})
+
+@app.route('/', methods=['GET'])
+def greetings():
+    return ("Hello, world!")
+
+if __name__=="__main__":
+    app.run(debug=True)
