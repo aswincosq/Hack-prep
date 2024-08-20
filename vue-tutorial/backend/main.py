7589c8b856ec474b009270cc96610e419dfd85ba
AllenTJ7 <163137620+AllenTJ7@users.noreply.github.com>
2024-08-20 15:50:15 +0530

    changes

--- vue-tutorial/backend/main.py
+++ vue-tutorial/backend/main.py
@@ -7,9 +7,49 @@ app.config.from_object(__name__)
 
 CORS(app,resources={r"/*":{'origins':"*"}})
 
-@app.route('/', methods=['GET'])
-def greetings():
-    return ("Hello, world!")
+@app.route('/shark', methods=['GET'])
+def shark():
+    return ("Hello, this is a freaking good shark")
+
+Games = [
+    {
+        'title':'GTA V',
+        'genre':'action-adventure',
+        'played':True,
+    },
+    {
+        'title':'the last of us',
+        'genre':'survival',
+        'played':False,
+    },
+    {
+        'title':'RDR 2',
+        'genre':'action-adventure',
+        'played':True,
+    },
+    {
+        'title':'Batman:Arkham Knight',
+        'genre':'fighting',
+        'played':True,
+    },
+    {
+        'title':'God of War:Ragnarok',
+        'genre':'hack-and-slash',
+        'played':False,
+    },
+    {
+        'title':'Spider-Man',
+        'genre':'Sci-Fi',
+        'played':False,
+    }
+]
+
+@app.route('/games', methods=['GET'])
+def all_games():
+    return jsonify({
+        'games': Games,
+        'status': 'success'
+    })
 
 if __name__=="__main__":
     app.run(debug=True)
